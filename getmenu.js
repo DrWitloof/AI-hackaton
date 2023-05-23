cf34bc0cc5079f23a15588ba4166fa75807ee202
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2023-05-23 12:17:14 +0200

    Update getmenu.js

--- getmenu.js
+++ getmenu.js
@@ -1,1 +1,23 @@
+function getProductName(element) {
+  if (!element) { return null; }
+  if (element.classList.contains("c-product-name__long")) { return element.textContent.trim(); }
 
+  var children = element.children;
+  for (var i = 0; i < children.length; i++) {
+    var child = children[i];
+    var foundElement = getProductName(child);
+    if (foundElement) { return foundElement; }
+  }
+  return null;
+}
+
+function findPlpItem(element) {
+  let currentElement = element;
+  while (currentElement) {
+    if (currentElement.classList && currentElement.classList.contains('plp-item')) {
+      return currentElement;
+    }
+    currentElement = currentElement.parentElement;
+  }
+  return null;
+}
