4ab3347f12cf5f7f434ac40a878eeb8236fbe124
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2023-05-23 12:24:51 +0200

    Update getmenu.js

--- getmenu.js
+++ getmenu.js
@@ -1,3 +1,30 @@
+function openAI(callbackfunction, apiKey, prompt) {
+  const apiUrl = 'https://api.openai.com/v1/completions';
+
+  GM_xmlhttpRequest({
+    method: 'POST',
+    url: apiUrl,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    data: JSON.stringify({
+      'prompt': prompt,
+      'max_tokens': 500,
+      'temperature': 0.6,
+      'model': 'text-davinci-002'
+    }),
+    onload: function (response) {
+      const responseData = JSON.parse(response.responseText);
+
+      callbackfunction(responseData.choices[0].text);
+    },
+    onerror: function (error) {
+      console.error('Er is een fout opgetreden bij het genereren van het recept:', error);
+    },
+  });
+}
+
 function getProductName(element) {
   if (!element) { return null; }
   if (element.classList.contains("c-product-name__long")) { return element.textContent.trim(); }
