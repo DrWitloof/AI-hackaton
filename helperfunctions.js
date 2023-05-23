4ebaabb27b4914a50d0008d2c33e984c10b48182
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2023-05-23 20:58:01 +0200

    Create helperfunctions.js

---
+++ helperfunctions.js
@@ -0,0 +1,27 @@
+
+function getProductName(element) { getSpecificClassValue(element, "c-product-name__long"); }
+function getProductBrand(element) { getSpecificClassValue(element, "c-product-name__brand"); }
+  
+function getSpecificClassValue(element, classname) {
+  if (!element) { return null; }
+  if (element.classList.contains(classname)) { return element.textContent.trim(); }
+
+  var children = element.children;
+  for (var i = 0; i < children.length; i++) {
+    var child = children[i];
+    var foundElement = getProductName(child);
+    if (foundElement) { return foundElement; }
+  }
+  return null;
+}
+
+function findPlpItem(element) {
+  let currentElement = element;
+  while (currentElement) {
+    if (currentElement.classList && currentElement.classList.contains('plp-item')) {
+      return currentElement;
+    }
+    currentElement = currentElement.parentElement;
+  }
+  return null;
+}
