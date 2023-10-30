ca443172a83a6a55b96ea9b3569f84087b7133f1
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2023-10-30 12:58:25 +0100

    Update openAI.js

--- openAI.js
+++ openAI.js
@@ -1,6 +1,5 @@
 function openAI(callbackfunction, prompt_p, apiKey_p) {
-  console.log("openAI(" + callbackfunction + ", " + prompt_p + ", " + apiKey_p + ")");
-  
+  console.log("openAI(" + callbackfunction + ", " + prompt_p + ", " + apiKey_p + ")");  
   const apiUrl = 'https://api.openai.com/v1/completions';
 
     GM_xmlhttpRequest({
