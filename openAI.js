3d89c21f896c02d2a6e2d428e846169fa5737197
Koen Van Der Auwera <koen.vanderauwera@gmail.com>
2023-10-30 12:56:09 +0100

    Update openAI.js

--- openAI.js
+++ openAI.js
@@ -3,7 +3,7 @@ function openAI(callbackfunction, prompt_p, apiKey_p) {
   
   const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-  GM_xmlhttpRequest({
+    GM_xmlhttpRequest({
     method: 'POST',
     url: apiUrl,
     headers: {
@@ -11,16 +11,16 @@ function openAI(callbackfunction, prompt_p, apiKey_p) {
       'Authorization': `Bearer ${apiKey_p}`
     },
     data: JSON.stringify({
+      'model': 'gpt-3.5-turbo-instruct',
       'prompt': prompt_p,
       'max_tokens': 500,
-      'temperature': 0.6,
-      'model': 'gpt-3.5-turbo' 
+      'temperature': 0.6
     }),
     onload: function (response) {
       console.log(response);
       if (response.status === 200) {
         const responseData = JSON.parse(response.responseText);
-        callbackfunction(responseData.choices[0].text);
+        callbackfunction(responseData.choices[0].text.trim());
       } else {
         console.error('Fout bij het ophalen van de respons. Statuscode:', response.status);
         console.error(response.responseText);
